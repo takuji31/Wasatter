75eb1e17d2df5ab036901b9f15ec4f41a1aaed09
Nishibayashi Takuji <takuji@senchan.jp>
2010-12-12 01:43:45 +0900

    初期設定ウィザード1ページ目追加

---
+++ src/jp/senchan/android/wasatter/setup/SetupMain.java
@@ -0,0 +1,13 @@
+package jp.senchan.android.wasatter.setup;
+
+import jp.senchan.android.wasatter.R;
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SetupMain extends Activity {
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.setup_main);
+	}
+}
