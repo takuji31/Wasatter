71fb5caf31c129598c7ddbf2a59934e95e7f5fea
Nishibayashi Takuji <takuji@senchan.jp>
2010-12-12 16:11:37 +0900

    初期設定ウィザード共通の親クラス追加

--- src/jp/senchan/android/wasatter/setup/SetupActivity.java
+++ src/jp/senchan/android/wasatter/setup/SetupActivity.java
@@ -1,12 +1,17 @@
 package jp.senchan.android.wasatter.setup;
 
-import jp.senchan.android.wasatter.R;
+import android.app.Activity;
 import android.os.Bundle;
 
-public class SetupMain extends SetupActivity {
+public class SetupActivity extends Activity {
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.setup_main);
+		this.initialize();
+	}
+	
+	protected void initialize() {
+		//TODO チュートリアル共通の処理
 	}
 }
