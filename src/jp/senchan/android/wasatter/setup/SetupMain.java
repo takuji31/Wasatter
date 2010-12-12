5a9a37eb5f9c9d364d00aa4977fb682c548e53c5
Nishibayashi Takuji <takuji@senchan.jp>
2010-12-12 18:49:02 +0900

    ページ進む処理を追加

--- src/jp/senchan/android/wasatter/setup/SetupMain.java
+++ src/jp/senchan/android/wasatter/setup/SetupMain.java
@@ -3,42 +3,71 @@ package jp.senchan.android.wasatter.setup;
 import jp.senchan.android.wasatter.R;
 import jp.senchan.android.wasatter.activity.Main;
 import jp.senchan.android.wasatter.setting.Setting;
+import jp.senchan.android.wasatter.util.ToastUtil;
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.Button;
 
-public class SetupMain extends SetupActivity {
+public class SetupMain extends Activity {
 	private static final String KEY_PAGE = "SETUP_PAGE";
-	private static final int MAX_PAGE = 100;
+	private static final String KEY_COMPLETED = "SETUP_COMPLETED_v_2_0";
+	private static final int MAX_PAGE = 5;
+	private int currentPage;
 	private Button prevButton;
 	private Button nextButton;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.setup_main);
-		this.progressSetup();
 		prevButton = (Button) findViewById(R.id.prevButton);
 		nextButton = (Button) findViewById(R.id.nextButton);
+		currentPage = Setting.get(SetupMain.KEY_PAGE, 1);
+		this.progressSetup();
+		nextButton.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				// 次のページへ進む処理
+				currentPage++;
+				SetupMain.this.progressSetup();
+			}
+		});
+		prevButton.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				// 前のページへ戻る処理
+				currentPage--;
+				SetupMain.this.progressSetup();
+			}
+		});
 	}
-	
+
 	protected void progressSetup() {
-		int currentPage = Setting.get(SetupMain.KEY_PAGE, 1);
-		if( currentPage >= SetupMain.MAX_PAGE ){
-			//最後のページまで完了していたら終わり
+		// 最後のページまで進んだか、既に完了していたら終わりにする
+		if (currentPage > SetupMain.MAX_PAGE || !Setting.get(SetupMain.KEY_COMPLETED, false) ) {
 			Intent gotoMain = new Intent(this, Main.class);
 			startActivity(gotoMain);
+			//ウィザードが最後まで完了したことを設定ファイルに格納しておく
+			Setting.set(SetupMain.KEY_COMPLETED, true);
+			//Activity終了
+			this.finish();
 			return;
 		}
-		
+
 		prevButton.setVisibility(View.VISIBLE);
-		switch(currentPage) {
-			case 1:
-				//初期ページ
-				
-				//戻るボタンを無効にする
-				prevButton.setVisibility(View.GONE);
+		switch (currentPage) {
+		case 1:
+			// 初期ページ
+
+			// 戻るボタンを無効にする
+			prevButton.setVisibility(View.GONE);
 			break;
 		}
+
+		// どこまで進んだかを設定に保存する
+		Setting.set(SetupMain.KEY_PAGE, currentPage);
 	}
 }
