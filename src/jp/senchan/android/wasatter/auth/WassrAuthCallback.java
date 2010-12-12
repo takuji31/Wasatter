c87e82bc84bede31d8fcd66463c7a0e356ad86aa
Nishibayashi Takuji <takuji@senchan.jp>
2010-12-12 20:34:07 +0900

    Wassr認証APIのコールバックURLを受け取るようにする

---
+++ src/jp/senchan/android/wasatter/auth/WassrAuthCallback.java
@@ -0,0 +1,12 @@
+package jp.senchan.android.wasatter.auth;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class WassrAuthCallback extends Activity{
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+	}
+}
