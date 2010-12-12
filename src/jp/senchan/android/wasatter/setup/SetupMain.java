6d3832c87ec638848c9bc0a9d79477ccb50b2620
Nishibayashi Takuji <takuji@senchan.jp>
2010-12-12 19:59:09 +0900

    Wassrアカウントの設定画面追加

--- src/jp/senchan/android/wasatter/setup/SetupMain.java
+++ src/jp/senchan/android/wasatter/setup/SetupMain.java
@@ -2,14 +2,17 @@ package jp.senchan.android.wasatter.setup;
 
 import jp.senchan.android.wasatter.R;
 import jp.senchan.android.wasatter.activity.Main;
+import jp.senchan.android.wasatter.auth.params.Wassr;
 import jp.senchan.android.wasatter.setting.Setting;
-import jp.senchan.android.wasatter.util.ToastUtil;
+import jp.senchan.android.wasatter.setting.WassrAccount;
 import android.app.Activity;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class SetupMain extends Activity {
 	private static final String KEY_PAGE = "SETUP_PAGE";
@@ -19,13 +22,18 @@ public class SetupMain extends Activity {
 	private int currentPage;
 	private Button prevButton;
 	private Button nextButton;
+	private Button gotoButton;
+	private TextView wizardText;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.setup_main);
+		this.checkAlreadyInstalled();
 		prevButton = (Button) findViewById(R.id.prevButton);
 		nextButton = (Button) findViewById(R.id.nextButton);
+		gotoButton = (Button) findViewById(R.id.gotoButton);
+		wizardText = (TextView) findViewById(R.id.wizardText);
 		currentPage = Setting.get(SetupMain.KEY_PAGE, 1);
 		this.progressSetup();
 		nextButton.setOnClickListener(new OnClickListener() {
@@ -48,37 +56,53 @@ public class SetupMain extends Activity {
 
 	protected void progressSetup() {
 		// 最後のページまで進んだか、既に完了していたら終わりにする
-		if (currentPage > SetupMain.MAX_PAGE || !Setting.get(SetupMain.KEY_COMPLETED, false) ) {
+		if (currentPage > SetupMain.MAX_PAGE
+				|| Setting.get(SetupMain.KEY_COMPLETED, false)) {
 			Intent gotoMain = new Intent(this, Main.class);
 			startActivity(gotoMain);
-			//ウィザードが最後まで完了したことを設定ファイルに格納しておく
+			// ウィザードが最後まで完了したことを設定ファイルに格納しておく
 			Setting.set(SetupMain.KEY_COMPLETED, true);
-			//Activity終了
+			// Activity終了
 			this.finish();
 			return;
 		}
 
 		prevButton.setVisibility(View.VISIBLE);
+		gotoButton.setVisibility(View.GONE);
 		switch (currentPage) {
-		case 1:
-			// 初期ページ
-
-			// 戻るボタンを無効にする
-			prevButton.setVisibility(View.GONE);
+			case 1:
+				// 初期ページ
+				// 戻るボタンを無効にする
+				prevButton.setVisibility(View.GONE);
+			break;
+			case 2:
+				//Wassrアカウント設定画面
+				gotoButton.setVisibility(View.VISIBLE);
+				gotoButton.setText(R.string.setup_wizard_button_goto_wassr_account);
+				wizardText.setText(R.string.setup_wizard_text_wassr_account);
+				gotoButton.setOnClickListener(new OnClickListener() {
+					@Override
+					public void onClick(View v) {
+						Uri wassrAuthUri = Uri.parse(Wassr.AUTH_URL);
+						Intent gotoWassrAuth = new Intent(Intent.ACTION_VIEW,wassrAuthUri);
+						startActivity(gotoWassrAuth);
+					}
+				});
 			break;
 		}
 
 		// どこまで進んだかを設定に保存する
 		Setting.set(SetupMain.KEY_PAGE, currentPage);
 	}
-	
+
 	protected void checkAlreadyInstalled() {
-		boolean alreadyInstalled = Setting.get(SetupMain.KEY_ALREADY_INSTALLED, false);
-		if(!alreadyInstalled){
-			//v1.0以前からのアップグレード、もしくは初期インストールの場合は設定を全部クリアする
+		boolean alreadyInstalled = Setting.get(SetupMain.KEY_ALREADY_INSTALLED,
+				false);
+		if (!alreadyInstalled) {
+			// v1.0以前からのアップグレード、もしくは初期インストールの場合は設定を全部クリアする
 			Setting.clear();
 		}
-		//インストール済みということにする
+		// インストール済みということにする
 		Setting.set(SetupMain.KEY_ALREADY_INSTALLED, true);
 	}
 }
