dabf3acbaf2e98896b48312bc78bbddbbc7ce63e
Nishibayashi Takuji <takuji@senchan.jp>
2011-03-24 08:10:47 +0900

    initial commit

---
+++ src/jp/senchan/android/wasatter/ActivityMain.java
@@ -0,0 +1,13 @@
+package jp.senchan.android.wasatter;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ActivityMain extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
