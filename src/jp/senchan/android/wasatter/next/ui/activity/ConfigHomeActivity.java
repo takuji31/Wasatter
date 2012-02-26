c68d0619be81eb634fb22087a3089c210eabc969
Nishibayashi Takuji <takuji@senchan.jp>
2012-02-26 23:42:52 +0900

    FragmentTransactionをcommitしてなかった

--- src/jp/senchan/android/wasatter/next/ui/activity/ConfigHomeActivity.java
+++ src/jp/senchan/android/wasatter/next/ui/activity/ConfigHomeActivity.java
@@ -21,6 +21,7 @@ public class ConfigHomeActivity extends BaseActivity implements OnPageTypeSelect
 		FragmentManager fm = getSupportFragmentManager();
 		FragmentTransaction ft = fm.beginTransaction();
 		ft.add(R.id.container, new PageTypesFragment());
+		ft.commit();
 	}
 
 	@Override
