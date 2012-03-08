ecb945b9a8116a7cd74a2c719d1b9a4a8405a1c6
Nishibayashi Takuji <takuji@senchan.jp>
2012-03-09 00:23:50 +0900

    アカウント一覧画面でAccountListAdapterを使うように

--- src/jp/senchan/android/wasatter/next/ui/fragment/AccountListFragment.java
+++ src/jp/senchan/android/wasatter/next/ui/fragment/AccountListFragment.java
@@ -1,11 +1,15 @@
 package jp.senchan.android.wasatter.next.ui.fragment;
 
+import java.util.List;
+
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ListView;
-import jp.senchan.android.wasatter.next.ui.adapter.WasadroidBaseAdapter;
+import jp.senchan.android.wasatter.next.model.Account;
+import jp.senchan.android.wasatter.next.model.dataobject.AccountData;
+import jp.senchan.android.wasatter.next.ui.adapter.AccountListAdapter;
 import jp.senchan.android.wasatter3.R;
 import jp.senchan.lib.ui.BaseFragment;
 
@@ -16,7 +20,8 @@ public class AccountListFragment extends BaseFragment {
 			Bundle savedInstanceState) {
 		View v = inflater.inflate(R.layout.account_list, null);
 		ListView listAccountList = (ListView) v.findViewById(R.id.account_list);
-		listAccountList.setAdapter(new WasadroidBaseAdapter(getActivity(), 0, null));
+		List<AccountData> accountList = new Account(getActivity()).getAccountList();
+		listAccountList.setAdapter(new AccountListAdapter(getActivity(), accountList));
 		return v;
 	}
 }
