9bdbb59ddcea218eceff49b06c74084753a60d41
Nishibayashi Takuji <takuji@senchan.jp>
2012-07-09 07:47:59 +0900

    使わないであろうクラスとレイアウトを削除

--- src/jp/senchan/android/wasatter/next/listener/OnPageTypeSelectListener.java
+++
@@ -1,5 +0,0 @@
-package jp.senchan.android.wasatter.next.listener;
-
-public interface OnPageTypeSelectListener {
-	public void onPageTypeSelect(String type);
-}
--- src/jp/senchan/android/wasatter/next/ui/fragment/SettingHomeFragment.java
+++
@@ -1,7 +0,0 @@
-package jp.senchan.android.wasatter.next.ui.fragment;
-
-import jp.senchan.android.wasatter.WasatterListFragment;
-
-public class SettingHomeFragment extends WasatterListFragment {
-
-}
