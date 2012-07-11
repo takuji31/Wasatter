18d83072f77292a5f3a2d1e083c37f143aab3165
Nishibayashi Takuji <takuji@senchan.jp>
2012-07-11 14:13:26 +0900

    Viewの生成処理入れた(未確認)

--- src/jp/senchan/android/wasatter/next/ui/adapter/TimelineAdapter.java
+++ src/jp/senchan/android/wasatter/next/ui/adapter/TimelineAdapter.java
@@ -2,10 +2,15 @@ package jp.senchan.android.wasatter.next.ui.adapter;
 
 import java.util.ArrayList;
 
+import com.androidquery.AQuery;
+
 import android.content.Context;
 import android.view.View;
+import android.widget.TextView;
 import jp.senchan.android.wasatter.R;
+import jp.senchan.android.wasatter.Wasatter;
 import jp.senchan.android.wasatter.next.model.api.WasatterStatus;
+import jp.senchan.android.wasatter.next.model.api.WassrStatus;
 import jp.senchan.lib.view.ArrayListAdapter;
 
 public class TimelineAdapter extends ArrayListAdapter<WasatterStatus> {
@@ -17,13 +22,20 @@ public class TimelineAdapter extends ArrayListAdapter<WasatterStatus> {
 	@Override
 	public int getViewLayoutId(int position) {
 		// TODO Auto-generated method stub
-		return R.layout.menu_listitem;
+		return R.layout.timeline_row;
 	}
 
 	@Override
 	public View createView(int position, WasatterStatus item, View v) {
-		// TODO Auto-generated method stub
-		return null;
+		
+		WassrStatus st = (WassrStatus) item;
+		AQuery aq = new AQuery(v);
+		aq.id(R.id.textViewName).text(st.user.screenName);
+		aq.id(R.id.textViewServiceName).text(Wasatter.SERVICE_WASSR);
+		aq.id(R.id.textViewBody).text(st.body);
+		aq.id(R.id.imageViewIcon).image(R.drawable.ic_default_user_icon).image(st.user.profileImageUrl);
+		
+		return v;
 	}
 
 }
