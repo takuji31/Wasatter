4307c91a8f27705183b5cbc683bab754ed94ed61
Nishibayashi Takuji <takuji@senchan.jp>
2012-08-05 16:31:22 +0900

    発言を投稿順に並べ替えるComparator

---
+++ src/jp/senchan/android/wasatter/utils/WasatterStatusComparator.java
@@ -0,0 +1,21 @@
+/**
+ *
+ */
+package jp.senchan.android.wasatter.utils;
+
+import java.util.Comparator;
+
+import jp.senchan.android.wasatter.WasatterItem;
+import jp.senchan.android.wasatter.model.api.WasatterStatus;
+
+/**
+ * @author Senka/Takuji
+ * 
+ */
+public class WasatterStatusComparator implements Comparator<WasatterStatus> {
+	@Override
+	public int compare(WasatterStatus lhs, WasatterStatus rhs) {
+        return lhs.getTime() > rhs.getTime() ? -1 : 1;
+	}
+
+}
