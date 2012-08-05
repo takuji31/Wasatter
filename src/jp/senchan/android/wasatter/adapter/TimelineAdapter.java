4a91d056f97b8d9a0e0933280ead5d9bd680cd0d
Nishibayashi Takuji <takuji@senchan.jp>
2012-08-05 16:27:51 +0900

    新しいタイムラインに投稿日時を表示

--- src/jp/senchan/android/wasatter/adapter/TimelineAdapter.java
+++ src/jp/senchan/android/wasatter/adapter/TimelineAdapter.java
@@ -1,6 +1,8 @@
 package jp.senchan.android.wasatter.adapter;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 
 import com.androidquery.AQuery;
 
@@ -9,7 +11,8 @@ import android.view.View;
 import jp.senchan.android.wasatter.R;
 import jp.senchan.android.wasatter.Wasatter;
 import jp.senchan.android.wasatter.model.api.WasatterStatus;
-import jp.senchan.android.wasatter.model.api.WassrStatus;
+import jp.senchan.android.wasatter.model.api.WasatterUser;
+import jp.senchan.android.wasatter.model.api.impl.wassr.WassrStatus;
 import jp.senchan.lib.view.ArrayListAdapter;
 
 public class TimelineAdapter extends ArrayListAdapter<WasatterStatus> {
@@ -29,11 +32,14 @@ public class TimelineAdapter extends ArrayListAdapter<WasatterStatus> {
 		
 		WassrStatus st = (WassrStatus) item;
 		AQuery aq = new AQuery(v);
-		aq.id(R.id.textViewName).text(st.user.name);
-		aq.id(R.id.textViewId).text(st.screenName());
-		aq.id(R.id.textViewServiceName).text(Wasatter.SERVICE_WASSR);
-		aq.id(R.id.textViewBody).text(st.body);
-		aq.id(R.id.imageViewIcon).image(R.drawable.ic_default_user_icon).image(st.user.profileImageUrl);
+		WasatterUser user = st.getUser();
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy/MM/yy HH:mm::ss");
+		
+		aq.id(R.id.textViewName).text(user.getName());
+		aq.id(R.id.textViewServiceName).text(st.getServiceName());
+		aq.id(R.id.textViewBody).text(st.getBody());
+		aq.id(R.id.textViewCreatedAt).text(sdf.format(new Date(st.getTime())));
+		aq.id(R.id.imageViewIcon).image(R.drawable.ic_default_user_icon).image(user.getProfileImageUrl());
 		
 		return v;
 	}
