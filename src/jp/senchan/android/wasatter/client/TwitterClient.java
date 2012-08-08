cd5cf3a54240ec865569f18422454048025d7a84
Nishibayashi Takuji <takuji@senchan.jp>
2012-08-08 14:36:08 +0900

    TwitterClientの内部しょりをAsyncTwitterClientに合わせる

--- src/jp/senchan/android/wasatter/client/TwitterClient.java
+++ src/jp/senchan/android/wasatter/client/TwitterClient.java
@@ -1,45 +1,53 @@
 package jp.senchan.android.wasatter.client;
 
+import jp.senchan.android.wasatter.Wasatter;
 import jp.senchan.android.wasatter.auth.params.OAuthTwitter;
 import android.net.Uri;
+import android.text.TextUtils;
 import twitter4j.Twitter;
 import twitter4j.TwitterException;
 import twitter4j.TwitterFactory;
 import twitter4j.auth.AccessToken;
-import twitter4j.auth.OAuthAuthorization;
 import twitter4j.auth.RequestToken;
-import twitter4j.conf.Configuration;
 import twitter4j.conf.ConfigurationBuilder;
 
 public class TwitterClient {
 	
+	private TwitterFactory mFactory;
 	private RequestToken mRequestToken;
-	private OAuthAuthorization mOAuth;
-	protected Configuration conf;
+	private AccessToken mToken;
+	private Wasatter mApp;
 
-	public TwitterClient() {
+	public TwitterClient(Wasatter app) {
 		ConfigurationBuilder builder = new ConfigurationBuilder();
 		builder.setOAuthConsumerKey(OAuthTwitter.CONSUMER_KEY);
 		builder.setOAuthConsumerSecret(OAuthTwitter.CONSUMER_SECRET);
-		conf = builder.build();
-		mOAuth = new OAuthAuthorization(conf);
+		mApp = app;
+		mFactory = new TwitterFactory(builder.build());
+		fetchToken();
+	}
+	
+	public void fetchToken() {
+		String token = mApp.getTwitterToken();
+		String tokenSecret = mApp.getTwitterTokenSecret();
+		if (!TextUtils.isEmpty(token) && !TextUtils.isEmpty(tokenSecret)) {
+			mToken = new AccessToken(token, tokenSecret);
+		}
 	}
 	
 	public Twitter getClient() {
-		return new TwitterFactory(conf).getInstance();
+		fetchToken();
+		return mFactory.getInstance(mToken);
 	}
 	
-	public Twitter getClient(AccessToken token) {
-		return new TwitterFactory(conf).getInstance(token);
-	}
 
 	public String getAuthorizationURL () throws TwitterException {
-		mRequestToken = mOAuth.getOAuthRequestToken();
+		mRequestToken = getClient().getOAuthRequestToken();
 		return mRequestToken.getAuthorizationURL();
 	}
 	
 	public AccessToken getAccessTokenFromURL (Uri uri) throws TwitterException {
 		String verifier = uri.getQueryParameter("oauth_verifier");
-		return mOAuth.getOAuthAccessToken(mRequestToken, verifier);
+		return getClient().getOAuthAccessToken(mRequestToken, verifier);
 	}
 }
