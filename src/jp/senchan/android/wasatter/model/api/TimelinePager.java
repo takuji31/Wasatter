e8f62904c62579f7297b44fc07e061f7de670ba2
Nishibayashi Takuji <takuji@senchan.jp>
2012-08-12 05:38:30 +0900

    最後のリクエストのエラーコードをとるようにする

--- src/jp/senchan/android/wasatter/model/api/TimelinePager.java
+++ src/jp/senchan/android/wasatter/model/api/TimelinePager.java
@@ -6,5 +6,21 @@ public abstract class TimelinePager extends ArrayList<WasatterStatus> {
 
 	private static final long serialVersionUID = 1L;
 	
+	public static final int SUCCESS = 200;
+	public static final int FAILURE = -1;
+
+	private int mLastResultCode = -1;
+
+	protected int mMode;
+	
 	public abstract void loadNext();
+	
+	public void setLastResultCode(int result) {
+		mLastResultCode = result;
+	}
+
+	public int getLastResultCode() {
+		return mLastResultCode;
+	}
+	
 }
