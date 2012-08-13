409fa2663e11518e3a0bb85589e1d58926766653
Nishibayashi Takuji <takuji@senchan.jp>
2012-08-13 09:02:48 +0900

    イイネの情報を入れるようにした

--- src/jp/senchan/android/wasatter/model/api/impl/wassr/WassrStatus.java
+++ src/jp/senchan/android/wasatter/model/api/impl/wassr/WassrStatus.java
@@ -1,9 +1,12 @@
 package jp.senchan.android.wasatter.model.api.impl.wassr;
 
+import java.util.ArrayList;
+
 import jp.senchan.android.wasatter.Wasatter;
 import jp.senchan.android.wasatter.model.api.WasatterStatus;
 import jp.senchan.android.wasatter.model.api.WasatterUser;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
@@ -22,17 +25,27 @@ public class WassrStatus implements WasatterStatus {
 	public static final String KEY_USER = "user";
 	public static final String KEY_EPOCH = "epoch";
 	public static final String KEY_HTML = "html";
+	public static final String KEY_FAVORITES = "favorites";
 	
 	private String mRid;
 	private WassrUser mUser;
 	private long mEpoch;
 	private String mBody;
+	private ArrayList<String> mFavorites;
 	
 	public WassrStatus(JSONObject json) throws JSONException {
 		mRid = json.getString(KEY_RID);
 		mUser = new WassrUser(json.getJSONObject(KEY_USER), json.getString(KEY_LOGIN_ID));
 		mEpoch = json.getLong(KEY_EPOCH);
 		mBody = json.getString(KEY_HTML);
+		mFavorites = new ArrayList<String>();
+		JSONArray favorites = json.getJSONArray(KEY_FAVORITES);
+		if (favorites != null) {
+			int length = favorites.length();
+			for (int i = 0; i < length; i++) {
+				mFavorites.add(favorites.getString(i));
+			}
+		}
 	}
 
  	@Override
