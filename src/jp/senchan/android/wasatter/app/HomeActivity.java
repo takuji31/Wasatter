b0d392ed6f3616da5ca526309aba4ed859fac2b4
Nishibayashi Takuji <takuji@senchan.jp>
2012-08-14 12:06:25 +0900

    メニュー生成を自動化

--- src/jp/senchan/android/wasatter/app/HomeActivity.java
+++ src/jp/senchan/android/wasatter/app/HomeActivity.java
@@ -5,6 +5,7 @@ import java.util.Arrays;
 import com.actionbarsherlock.app.ActionBar;
 import com.actionbarsherlock.view.MenuItem;
 
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
@@ -24,26 +25,21 @@ public class HomeActivity extends WasatterActivity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.home);
 		mMenu = new SlideMenu(this);
-		//TODO 後でちゃんと書く
-		SlideMenuItem[] items = new SlideMenuItem[5];
-		// fill the menu-items here
-		items[0] = new SlideMenuItem();
-		items[1] = new SlideMenuItem();
-		items[2] = new SlideMenuItem();
-		items[3] = new SlideMenuItem();
-		items[4] = new SlideMenuItem();
-		items[0].label = "Profile";
-		items[0].icon = R.drawable.ic_action_profile;
-		items[1].label = "Timeline";
-		items[1].icon = R.drawable.ic_action_timeline;
-		items[2].label = "Reply";
-		items[2].icon = R.drawable.ic_action_reply;
-		items[3].label = "Odai";
-		items[3].icon = R.drawable.ic_action_odai;
-		items[4].label = "Channel";
-		items[4].icon = R.drawable.ic_action_channel;
-		ArrayList<SlideMenuItem> item = new ArrayList<SlideMenuItem>(Arrays.asList(items));
-		SlideMenuAdapter adapter = new SlideMenuAdapter(this, item);
+		
+		ArrayList<SlideMenuItem> items = new ArrayList<SlideMenuItem>();
+
+		Resources res = getResources();
+		String[] titles = res.getStringArray(R.array.slidemenu_title);
+		String[] icons = res.getStringArray(R.array.slidemenu_icon);
+		
+		for (int i = 0; i < titles.length; i++) {
+			SlideMenuItem item = new SlideMenuItem();
+			item.label = titles[i];
+			item.icon = res.getIdentifier(icons[i], "drawable", getPackageName());
+			items.add(item);
+		}
+		
+		SlideMenuAdapter adapter = new SlideMenuAdapter(this, items);
 		mMenu.setAdapter(adapter);
 		mMenu.checkEnabled();
 		
