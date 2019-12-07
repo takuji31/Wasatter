d17a1c3cabff38a77619015382306e56d9d6bcfe
Takuji Nishibayashi <takuji31@gmail.com>
2019-12-07 18:07:12 +0900

    remove ToastUtil overload function

--- app/src/main/java/jp/senchan/android/wasatter/utils/ToastUtil.kt
+++ app/src/main/java/jp/senchan/android/wasatter/utils/ToastUtil.kt
@@ -4,12 +4,7 @@ import android.widget.Toast
 import jp.senchan.android.wasatter.Wasatter
 
 object ToastUtil {
-    @JvmOverloads
     fun show(text: String?, duration: Int = Toast.LENGTH_SHORT) {
         Toast.makeText(Wasatter.CONTEXT, text, duration).show()
-    }
-
-    fun showLong(text: String?) {
-        show(text, Toast.LENGTH_LONG)
     }
 }
